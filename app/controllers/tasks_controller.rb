80a97a39fb837f2e177004f797e609261b5206cd
naoki <falcon2i.skyhigh@gmail.com>
2018-08-27 09:09:06 +0000

    fix error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,40 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
+  
+  def index
+    if logged_in?
+      @user = current_user
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
   
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクを投稿しました。'
-      redirect_to root_url
+      redirect_to @task
     else
-      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'toppages/index'
+      render :new
     end
   end
   
   def edit
-    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end
   
   def update
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
-      redirect_to root_url
+      redirect_to @task
     else
       flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
@@ -31,10 +44,14 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'タスクを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
   
   private
+  
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content, :status)
